2e593b1d6dac0609ad29c6839cd4f7b5abe00c8f
Nikita <68944906+BANanaD3V@users.noreply.github.com>
2023-12-02 15:53:57 +0300

    fix spicetify

--- home/software/spotify.nix
+++ home/software/spotify.nix
@@ -5,15 +5,9 @@
   spicetify-nix,
   ...
 }: let
-  flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
-  spicetify-nix = (import flake-compat {src = builtins.fetchTarball "https://github.com/the-argus/spicetify-nix/archive/master.tar.gz";}).defaultNix;
-  spicePkgs = spicetify-nix.packages.${pkgs.system}.default;
-  officialThemes = pkgs.fetchgit {
-    url = "https://github.com/spicetify/spicetify-themes";
-    sha256 = "sha256-iMRutzOrZHaKfHOr2qOM/TkTYHysFBlmunVhU65kf2k=";
-  };
+  spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
 in {
-  imports = [spicetify-nix.homeManagerModule];
+  imports = [inputs.spicetify-nix.homeManagerModule];
   programs.spicetify = {
     enable = true;
     theme = spicePkgs.themes.text;
