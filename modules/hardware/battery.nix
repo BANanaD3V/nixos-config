9303bdeda96fd09f7fc7179d9224b3531b7d6e75
Nikita <68944906+BANanaD3V@users.noreply.github.com>
2023-12-05 14:12:53 +0300

    battery x3

--- modules/hardware/battery.nix
+++ modules/hardware/battery.nix
@@ -12,6 +12,7 @@
     # Better scheduling for CPU cycles
     services.system76-scheduler.settings.cfsProfiles.enable = true;
 
+    # Auto cpu frequency
     programs.auto-cpufreq = {
       enable = true;
       settings = {
@@ -40,11 +41,11 @@
 
     # Disable GNOMEs power management
     services.power-profiles-daemon.enable = false;
-    #
-    # # Enable powertop
+
+    # Enable powertop
     powerManagement.powertop.enable = lib.mkForce true;
-    #
-    # # Enable thermald (only necessary if on Intel CPUs)
+
+    # Enable thermald (only necessary if on Intel CPUs)
     services.thermald.enable = true;
   };
 }
