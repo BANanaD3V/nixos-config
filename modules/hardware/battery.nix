1ea3a7d7661696191fc654e72f7a877bfe4c1bcc
Nikita <68944906+BANanaD3V@users.noreply.github.com>
2023-12-05 14:11:24 +0300

    more battery optimizations

--- modules/hardware/battery.nix
+++ modules/hardware/battery.nix
@@ -10,7 +10,7 @@
 
   config = lib.mkIf config.hm.banana-hm.battery.enable {
     # Better scheduling for CPU cycles
-    # services.system76-scheduler.settings.cfsProfiles.enable = true;
+    services.system76-scheduler.settings.cfsProfiles.enable = true;
 
     programs.auto-cpufreq = {
       enable = true;
@@ -26,24 +26,25 @@
         };
       };
     };
+
     # Enable TLP (better than gnomes internal power manager)
-    # services.tlp = {
-    #   enable = true;
-    #   settings = {
-    #     CPU_BOOST_ON_AC = 1;
-    #     CPU_BOOST_ON_BAT = 0;
-    #     CPU_SCALING_GOVERNOR_ON_AC = "performance";
-    #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-    #   };
-    # };
+    services.tlp = {
+      enable = true;
+      settings = {
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      };
+    };
 
     # Disable GNOMEs power management
-    # services.power-profiles-daemon.enable = false;
+    services.power-profiles-daemon.enable = false;
     #
     # # Enable powertop
-    # powerManagement.powertop.enable = lib.mkForce true;
+    powerManagement.powertop.enable = lib.mkForce true;
     #
     # # Enable thermald (only necessary if on Intel CPUs)
-    # services.thermald.enable = true;
+    services.thermald.enable = true;
   };
 }
