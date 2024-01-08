6becf50a3468c4f749aeaa553b44d4233d8d5751
Nikita <banana@banana.is-cool.dev>
2024-01-08 19:22:02 +0300

    fix uploader once again

--- home/desktop/hyprland/scripts/uploader.nix
+++ home/desktop/hyprland/scripts/uploader.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "uploader" ''
   file=$(find $HOME -type f | rofi -dmenu -config ~/.config/rofi/configs/clipboard.rasi -p '>')
-  url=$(curl -F "file=@"$file" -Fsecret= 0x0.banana.is-cool.dev)
+  url=$(curl -F "file=@$file" -Fsecret= 0x0.banana.is-cool.dev)
   wl-copy $url
   notify-send "File has been uploaded
   The url is $url"
