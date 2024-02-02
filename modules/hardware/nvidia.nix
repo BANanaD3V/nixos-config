93226be8fb563068bccf94d4b6436396afdee75d
Nikita <68944906+BANanaD3V@users.noreply.github.com>
2024-02-02 18:17:59 +0300

    update flake

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -3,7 +3,16 @@
   lib,
   pkgs,
   ...
-}: {
+}:
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
+in {
   config = lib.mkIf config.nixos.nvidia.drivers.enable {
     # Make sure opengl is enabled
     hardware.opengl = {
@@ -32,6 +41,15 @@
 
       # Enable the nvidia settings menu
       nvidiaSettings = true;
+      prime = lib.mkIf config.hm.home-manager.battery.enable {
+       offload.enable = true;
+
+       # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+       nvidiaBusId = "PCI:01:0:0";
+
+       # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+       amdgpuBusId = "PCI:14:0:0";
+     };
     };
     environment.sessionVariables = {
       NIXOS_OZONE_WL= "1";
@@ -45,5 +63,6 @@
       __GL_GSYNC_ALLOWED = "1";
       __GL_VRR_ALLOWED = "0";
     };
+    environment.systemPackages = [ nvidia-offload ];
   };
 }
